f540da00a0f0c1120bf1ed4b1df86d05abee208b
Douglas Mill <dmill989@gmail.com>
2013-11-09 11:46:34 -0800

    adds room name input to home view

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -2,6 +2,11 @@ Template.home.events({
   'click button': function() {
 
     var permalink = Random.id();
+    if($('#room-name').val() !== "Room Name") {
+      var roomName = $('#room-name').val();
+      } else {
+        var roomName = permalink;
+    };
     var chatroomId = Chatrooms.insert({people: [], permalink: permalink});
 
     var personId = Random.id();
@@ -11,5 +16,8 @@ Template.home.events({
     //Chatrooms.update(chatroomId, { $push: { people: person }});
 
     Meteor.Router.to("/room/" + permalink);
+  },
+  'click #room-name': function() {
+    $('#room-name').val("")
   }
 });
